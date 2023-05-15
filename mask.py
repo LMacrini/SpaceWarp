d555bc127b6cf15368a028c123ca6083d82d576d
Wam25 <liomacrini@gmail.com>
2023-05-15 16:04:12 -0400

    made a mask.py file to generate the mask and put it in mask.json

---
+++ mask.py
@@ -0,0 +1,31 @@
+import pyxel
+import json
+
+class App:
+    def __init__(self, room_nb):
+        self.room_nb = room_nb
+        self.rooms = []
+        pyxel.init(128, 128, title='SpaceWarp')
+        pyxel.load("assets/1.pyxres")
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+
+        if 0 < pyxel.frame_count <= self.room_nb:
+            room = [[pyxel.pget(x * 8, y * 8) for y in range(16)] for x in range(16)]
+            self.rooms.append(room)
+
+        if pyxel.frame_count == self.room_nb:
+            with open('mask.json', 'w') as file:
+                self.rooms_json = json.dumps(self.rooms)
+                file.write(self.rooms_json)
+
+
+        elif pyxel.frame_count == self.room_nb + 1:
+            pyxel.quit()
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.bltm(0, 0, 2, 128 * (pyxel.frame_count), 128, 128, 128)
+
+App(int(input("Number of rooms?")))
