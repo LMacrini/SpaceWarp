e82c541c16f423dfb0cb5764074a44cdb8a3f199
Wam25 <62901118+Wam25@users.noreply.github.com>
2023-05-21 19:46:30 -0400

    fix mask.py

--- mask.py
+++ mask.py
@@ -4,6 +4,7 @@ import json
 class App:
     def __init__(self, room_nb):
         self.room_nb = room_nb
+        self.frame_delay = 30
         self.rooms = []
         pyxel.init(128, 128, title='SpaceWarp')
         pyxel.load("ressources/assets.pyxres")
@@ -11,20 +12,22 @@ class App:
 
     def update(self):
 
-        if 0 < pyxel.frame_count <= self.room_nb:
+        if 0 < pyxel.frame_count // self.frame_delay <= self.room_nb and pyxel.frame_count % self.frame_delay == 0:
             room = [[pyxel.pget(x * 8, y * 8) for y in range(16)] for x in range(16)]
+            # print(room)
             self.rooms.append(room)
 
-        if pyxel.frame_count == self.room_nb:
+        if pyxel.frame_count // self.frame_delay == self.room_nb and pyxel.frame_count % self.frame_delay == 0:
+            print(self.rooms)
             with open('ressources/mask.json', 'w') as file:
                     json.dump(self.rooms, file)
 
 
-        elif pyxel.frame_count == self.room_nb + 1:
+        elif pyxel.frame_count // self.frame_delay == self.room_nb + 1:
             pyxel.quit()
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.bltm(0, 0, 2, 128 * (pyxel.frame_count), 128, 128, 128)
+        pyxel.bltm(0, 0, 2, 128 * (pyxel.frame_count // 30), 128, 128, 128)
 
 App(int(input("Number of rooms?")))
